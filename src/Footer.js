5de2aa9fec4b9914ed9fd0270ee4657cd20e1eb4
Rey Mercado <reymercado.usa@gmail.com>
2022-01-10 23:00:28 -0800

    Bare minimum for Lab-Class01

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <footer>
+        <p>&copy;2022 Rey Mercado</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
